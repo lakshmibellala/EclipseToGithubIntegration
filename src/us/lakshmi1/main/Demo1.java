71aad2c48aa89eb3ce06256b9adf2232fcbc4522
lakshmibellala <lakshmi_bellala@yahoo.com>
2022-11-14 23:15:27 -0500

    First commit

---
+++ src/us/lakshmi1/main/Demo1.java
@@ -0,0 +1,12 @@
+package us.lakshmi1.main;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello, how arer you?";
+		String[] arr=s.split(" ");
+		System.out.println(String.join("_", arr));
+	}
+
+}
